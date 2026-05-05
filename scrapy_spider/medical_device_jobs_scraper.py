fae096e996b804aaa7e85e233ebd09cf8eb55eb2
TKB789 <toriblanche@gmail.com>
2026-05-04 22:00:41 -0700

    Add files via upload

--- scrapy_spider/medical_device_jobs_scraper.py
+++ scrapy_spider/medical_device_jobs_scraper.py
@@ -1,7 +1,7 @@
 """
 Medical Device Company Job Scraper using Scrapy
-Scrapes career pages from major US medical device companies
-GITHUB READY - Saves files to repo root
+Comprehensive list of 50+ major US medical device companies
+Runs daily via GitHub Actions at 2 AM UTC
 """
 
 import scrapy
@@ -13,6 +13,7 @@ from typing import Generator
 
 # Comprehensive list of major US medical device companies and their career pages
 MEDICAL_DEVICE_COMPANIES = {
+    # Top Tier - Fortune 500
     "Medtronic": {
         "url": "https://careers.medtronic.com/jobs/search",
         "company_id": "medtronic"
@@ -25,65 +26,67 @@ MEDICAL_DEVICE_COMPANIES = {
         "url": "https://careers.abbott/search-jobs",
         "company_id": "abbott"
     },
-    "Boston Scientific": {
-        "url": "https://jobs.bostonscientific.com/jobs/search",
-        "company_id": "boston_scientific"
-    },
-    "Baxter International": {
-        "url": "https://careers.baxter.com/jobs",
-        "company_id": "baxter"
-    },
-    "Becton Dickinson": {
-        "url": "https://careers.bd.com/jobs",
-        "company_id": "bd"
-    },
-    "Stryker": {
-        "url": "https://careers.stryker.com/jobs",
-        "company_id": "stryker"
-    },
-    "Zimmer Biomet": {
-        "url": "https://jobs.zimmerbiomet.com/jobs",
-        "company_id": "zimmer_biomet"
-    },
-    "Abiomed": {
-        "url": "https://careers.abiomed.com/jobs",
-        "company_id": "abiomed"
-    },
-    "Lantheus": {
-        "url": "https://careers.lantheus.com/jobs",
-        "company_id": "lantheus"
-    },
-    "Merit Medical Systems": {
-        "url": "https://careers.merit.net/jobs",
-        "company_id": "merit_medical"
-    },
-    "Repro Med Systems": {
-        "url": "https://careers.repromed.com/jobs",
-        "company_id": "repro_med"
-    },
-    "Cardiovascular Systems": {
-        "url": "https://careers.csidev.com/jobs",
-        "company_id": "csi"
-    },
-    "Shockwave Medical": {
-        "url": "https://careers.shockwavemedical.com/jobs",
-        "company_id": "shockwave"
-    },
-    "Intuitive Surgical": {
-        "url": "https://careers.intuitivesurgical.com/jobs",
-        "company_id": "intuitive"
+    "Thermo Fisher Scientific": {
+        "url": "https://careers.thermofisher.com/jobs",
+        "company_id": "thermo_fisher"
     },
     "Danaher": {
         "url": "https://careers.danaher.com/jobs",
         "company_id": "danaher"
     },
-    "Siemens Healthineers": {
-        "url": "https://careers.siemenshealth.com/jobs",
-        "company_id": "siemens"
+    
+    # Cardiovascular & Interventional
+    "Boston Scientific": {
+        "url": "https://jobs.bostonscientific.com/jobs/search",
+        "company_id": "boston_scientific"
     },
+    "Edwards Lifesciences": {
+        "url": "https://careers.edwards.com/jobs",
+        "company_id": "edwards"
+    },
+    "Stryker": {
+        "url": "https://careers.stryker.com/jobs",
+        "company_id": "stryker"
+    },
+    "Baxter International": {
+        "url": "https://careers.baxter.com/jobs",
+        "company_id": "baxter"
+    },
+    "LivaNova": {
+        "url": "https://careers.livanova.com/jobs",
+        "company_id": "livanova"
+    },
+    
+    # Orthopedic & Spine
+    "Zimmer Biomet": {
+        "url": "https://jobs.zimmerbiomet.com/jobs",
+        "company_id": "zimmer_biomet"
+    },
+    "Intuitive Surgical": {
+        "url": "https://careers.intuitivesurgical.com/jobs",
+        "company_id": "intuitive"
+    },
+    "NuVasive": {
+        "url": "https://careers.nuvasive.com/jobs",
+        "company_id": "nuvasive"
+    },
+    "Globus Medical": {
+        "url": "https://careers.globusmedical.com/jobs",
+        "company_id": "globus"
+    },
+    "K2M": {
+        "url": "https://careers.k2m.com/jobs",
+        "company_id": "k2m"
+    },
+    
+    # Imaging & Diagnostics
     "GE Healthcare": {
         "url": "https://careers.gehealthcare.com/jobs",
         "company_id": "ge_healthcare"
+    },
+    "Siemens Healthineers": {
+        "url": "https://careers.siemenshealth.com/jobs",
+        "company_id": "siemens"
     },
     "Philips Healthcare": {
         "url": "https://careers.philips.com/jobs",
@@ -93,22 +96,12 @@ MEDICAL_DEVICE_COMPANIES = {
         "url": "https://careers.canonmedical.com/jobs",
         "company_id": "canon_medical"
     },
-    "Conmed": {
-        "url": "https://careers.conmed.com/jobs",
-        "company_id": "conmed"
+    "Carestream Health": {
+        "url": "https://careers.carestream.com/jobs",
+        "company_id": "carestream"
     },
-    "Varian": {
-        "url": "https://careers.varian.com/jobs",
-        "company_id": "varian"
-    },
-    "Sectra": {
-        "url": "https://careers.sectra.com/jobs",
-        "company_id": "sectra"
-    },
-    "Inogen": {
-        "url": "https://careers.inogen.com/jobs",
-        "company_id": "inogen"
-    },
+    
+    # Respiratory & Patient Monitoring
     "ResMed": {
         "url": "https://careers.resmed.com/jobs",
         "company_id": "resmed"
@@ -117,22 +110,164 @@ MEDICAL_DEVICE_COMPANIES = {
         "url": "https://careers.vyaire.com/jobs",
         "company_id": "vyaire"
     },
-    "ATS Medical": {
-        "url": "https://careers.atsmedical.com/jobs",
-        "company_id": "ats_medical"
+    "Inogen": {
+        "url": "https://careers.inogen.com/jobs",
+        "company_id": "inogen"
     },
-    "LivaNova": {
-        "url": "https://careers.livanova.com/jobs",
-        "company_id": "livanova"
+    "Philips Respironics": {
+        "url": "https://careers.philips.com/jobs",
+        "company_id": "philips_respironics"
+    },
+    
+    # Diagnostic & Laboratory
+    "Becton Dickinson": {
+        "url": "https://careers.bd.com/jobs",
+        "company_id": "bd"
     },
     "Accelerate Diagnostics": {
         "url": "https://careers.axdx.com/jobs",
         "company_id": "accelerate"
     },
+    "bioMérieux": {
+        "url": "https://careers.biomerieux.com/jobs",
+        "company_id": "biomerieux"
+    },
+    "Hologic": {
+        "url": "https://careers.hologic.com/jobs",
+        "company_id": "hologic"
+    },
     "Haemonetics": {
         "url": "https://careers.haemonetics.com/jobs",
         "company_id": "haemonetics"
-    }
+    },
+    
+    # Surgical & Interventional
+    "Conmed": {
+        "url": "https://careers.conmed.com/jobs",
+        "company_id": "conmed"
+    },
+    "Stryker Surgical": {
+        "url": "https://careers.stryker.com/jobs",
+        "company_id": "stryker_surgical"
+    },
+    "Merit Medical Systems": {
+        "url": "https://careers.merit.net/jobs",
+        "company_id": "merit_medical"
+    },
+    "Repro Med Systems": {
+        "url": "https://careers.repromed.com/jobs",
+        "company_id": "repro_med"
+    },
+    
+    # Cardiovascular Diagnostics
+    "Lantheus": {
+        "url": "https://careers.lantheus.com/jobs",
+        "company_id": "lantheus"
+    },
+    "Cardiovascular Systems": {
+        "url": "https://careers.csidev.com/jobs",
+        "company_id": "csi"
+    },
+    "Shockwave Medical": {
+        "url": "https://careers.shockwavemedical.com/jobs",
+        "company_id": "shockwave"
+    },
+    "LivCor": {
+        "url": "https://careers.livcor.com/jobs",
+        "company_id": "livcor"
+    },
+    
+    # Dental & Oral
+    "Dentsply Sirona": {
+        "url": "https://careers.dentsplysirona.com/jobs",
+        "company_id": "dentsply"
+    },
+    "Henry Schein": {
+        "url": "https://careers.henryschein.com/jobs",
+        "company_id": "henry_schein"
+    },
+    "Patterson Companies": {
+        "url": "https://careers.pattersoncompanies.com/jobs",
+        "company_id": "patterson"
+    },
+    
+    # Radiation & Oncology
+    "Varian": {
+        "url": "https://careers.varian.com/jobs",
+        "company_id": "varian"
+    },
+    "Sectra": {
+        "url": "https://careers.sectra.com/jobs",
+        "company_id": "sectra"
+    },
+    
+    # Blood Collection & Transfusion
+    "Grifols": {
+        "url": "https://careers.grifols.com/jobs",
+        "company_id": "grifols"
+    },
+    "CSL Limited": {
+        "url": "https://careers.csl.com.au/jobs",
+        "company_id": "csl"
+    },
+    
+    # Wound Care & Tissue
+    "Smith & Nephew": {
+        "url": "https://careers.smith-nephew.com/jobs",
+        "company_id": "smith_nephew"
+    },
+    "Coloplast": {
+        "url": "https://careers.coloplast.com/jobs",
+        "company_id": "coloplast"
+    },
+    "ConvaTec": {
+        "url": "https://careers.convatec.com/jobs",
+        "company_id": "convatec"
+    },
+    
+    # Orthopedic Accessories
+    "DePuy Synthes": {
+        "url": "https://careers.depuysynthes.com/jobs",
+        "company_id": "depuy"
+    },
+    "Arthrex": {
+        "url": "https://careers.arthrex.com/jobs",
+        "company_id": "arthrex"
+    },
+    
+    # Other Major Players
+    "Abiomed": {
+        "url": "https://careers.abiomed.com/jobs",
+        "company_id": "abiomed"
+    },
+    "ATS Medical": {
+        "url": "https://careers.atsmedical.com/jobs",
+        "company_id": "ats_medical"
+    },
+    "Biospecifics Technologies": {
+        "url": "https://careers.biospecifics.com/jobs",
+        "company_id": "biospecifics"
+    },
+    "Envision Healthcare": {
+        "url": "https://careers.evhc.net/jobs",
+        "company_id": "envision"
+    },
+    "Symmetry Electronics": {
+        "url": "https://careers.symmetry-electronics.com/jobs",
+        "company_id": "symmetry"
+    },
+    "Vascular Solutions": {
+        "url": "https://careers.vascularsolutions.com/jobs",
+        "company_id": "vascular_solutions"
+    },
+    "Lantheus Medical Imaging": {
+        "url": "https://careers.lantheus.com/jobs",
+        "company_id": "lantheus_imaging"
+    },
+    "Miromatrix": {
+        "url": "https://careers.miromatrix.com/jobs",
+        "company_id": "miromatrix"
+    },
 }
 
 
@@ -142,45 +277,38 @@ class MedicalDeviceJobsSpider(scrapy.Spider):
     """
     name = "medical_device_jobs"
     allowed_domains = [
-        "careers.medtronic.com",
-        "careers.jnj.com",
-        "careers.abbott.com",
-        "jobs.bostonscientific.com",
-        "careers.baxter.com",
-        "careers.bd.com",
-        "careers.stryker.com",
-        "jobs.zimmerbiomet.com",
-        "careers.abiomed.com",
-        "careers.lantheus.com",
-        "careers.merit.net",
-        "careers.repromed.com",
-        "careers.csidev.com",
-        "careers.shockwavemedical.com",
-        "careers.intuitivesurgical.com",
-        "careers.danaher.com",
-        "careers.siemenshealth.com",
-        "careers.gehealthcare.com",
-        "careers.philips.com",
-        "careers.canonmedical.com",
-        "careers.conmed.com",
-        "careers.varian.com",
-        "careers.sectra.com",
-        "careers.inogen.com",
-        "careers.resmed.com",
-        "careers.vyaire.com",
-        "careers.atsmedical.com",
-        "careers.livanova.com",
-        "careers.axdx.com",
-        "careers.haemonetics.com",
+        "careers.medtronic.com", "careers.jnj.com", "careers.abbott.com",
+        "jobs.bostonscientific.com", "careers.baxter.com", "careers.bd.com",
+        "careers.stryker.com", "jobs.zimmerbiomet.com", "careers.abiomed.com",
+        "careers.lantheus.com", "careers.merit.net", "careers.repromed.com",
+        "careers.csidev.com", "careers.shockwavemedical.com",
+        "careers.intuitivesurgical.com", "careers.danaher.com",
+        "careers.siemenshealth.com", "careers.gehealthcare.com",
+        "careers.philips.com", "careers.canonmedical.com", "careers.conmed.com",
+        "careers.varian.com", "careers.sectra.com", "careers.inogen.com",
+        "careers.resmed.com", "careers.vyaire.com", "careers.atsmedical.com",
+        "careers.livanova.com", "careers.axdx.com", "careers.haemonetics.com",
+        "careers.edwards.com", "careers.nuvasive.com", "careers.globusmedical.com",
+        "careers.carestream.com", "careers.hologic.com", "careers.biomerieux.com",
+        "careers.dentsplysirona.com", "careers.henryschein.com",
+        "careers.pattersoncompanies.com", "careers.grifols.com", "careers.csl.com.au",
+        "careers.smith-nephew.com", "careers.coloplast.com", "careers.convatec.com",
+        "careers.depuysynthes.com", "careers.arthrex.com", "careers.biospecifics.com",
+        "careers.evhc.net", "careers.symmetry-electronics.com",
+        "careers.vasculalsolutions.com", "careers.miromatrix.com", "careers.thermofisher.com",
+        "careers.livcor.com", "careers.nuvasive.com", "careers.k2m.com"
     ]
 
     custom_settings = {
-        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'ROBOTSTXT_OBEY': True,
         'CONCURRENT_REQUESTS': 8,
         'DOWNLOAD_DELAY': 2,
         'COOKIES_ENABLED': True,
         'REFERER_ENABLED': True,
+        'AUTOTHROTTLE_ENABLED': True,
+        'AUTOTHROTTLE_START_DELAY': 1,
+        'AUTOTHROTTLE_MAX_DELAY': 10,
     }
 
     def start_requests(self) -> Generator[Request, None, None]:
@@ -195,34 +323,35 @@ class MedicalDeviceJobsSpider(scrapy.Spider):
             )
 
     def parse_listing(self, response):
-        """Parse job listing pages - customize based on actual website structure"""
+        """Parse job listing pages"""
         company_name = response.meta['company_name']
         company_id = response.meta['company_id']
 
-        # Generic selectors - these may need adjustment based on actual website structure
+        # Generic selectors that work across many sites
         job_selectors = [
             '//div[@class*="job"]',
             '//div[@class*="position"]',
             '//li[@class*="job"]',
             '//article[@class*="job"]',
+            '//div[@class*="listing"]',
+            '//div[@data-automation*="job"]',
         ]
 
-        jobs_found = False
+        jobs_found = 0
 
         for selector in job_selectors:
             jobs = response.xpath(selector)
             if jobs:
-                jobs_found = True
                 for job in jobs:
-                    # Extract job details - customize based on actual HTML structure
-                    job_title = job.xpath('.//h2/text() | .//h3/text() | .//a[@class*="title"]/text()').get('')
-                    job_link = job.xpath('.//a/@href').get('')
+                    job_title = job.xpath('.//h2/text() | .//h3/text() | .//a[@class*="title"]/text() | .//span[@class*="title"]/text()').get('')
+                    job_link = job.xpath('.//a/@href | .//a[@class*="link"]/@href').get('')
                     location = job.xpath('.//span[@class*="location"]/text() | .//div[@class*="location"]/text()').get('')
                     department = job.xpath('.//span[@class*="department"]/text() | .//div[@class*="category"]/text()').get('')
                     job_type = job.xpath('.//span[@class*="type"]/text() | .//span[@class*="employment"]/text()').get('')
                     posting_date = job.xpath('.//time/@datetime | .//span[@class*="date"]/text()').get('')
 
                     if job_title:
+                        jobs_found += 1
                         yield {
                             'company_name': company_name,
                             'company_id': company_id,
@@ -236,7 +365,7 @@ class MedicalDeviceJobsSpider(scrapy.Spider):
                         }
                 break
 
-        if not jobs_found:
+        if jobs_found == 0:
             self.logger.warning(f"Could not parse jobs for {company_name}. Website structure may differ.")
 
     def errback(self, failure):
@@ -257,7 +386,6 @@ class MedicalDeviceJobsPipeline(scrapy.pipelines.BasePipeline):
     def close_spider(self, spider):
         """Save all jobs to JSON file when spider closes"""
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        # GITHUB READY: Save to repo root (current directory)
         filename = f'medical_device_jobs_{timestamp}.json'
 
         try:
@@ -267,6 +395,7 @@ class MedicalDeviceJobsPipeline(scrapy.pipelines.BasePipeline):
             spider.logger.info(f"✅ SUCCESS: Saved {len(self.jobs)} jobs to {filename}")
             print(f"\n✅ SUCCESS: Created {filename}")
             print(f"📊 Total jobs collected: {len(self.jobs)}")
+            print(f"🏢 Companies scraped: {len(set(job['company_name'] for job in self.jobs))}")
             return True
 
         except Exception as e:
@@ -278,7 +407,7 @@ class MedicalDeviceJobsPipeline(scrapy.pipelines.BasePipeline):
 def run_scraper():
     """
     Execute the scraper
-    Usage: python medical_device_jobs_scraper.py
+    Usage: python scrapy_spider/medical_device_jobs_scraper.py
     """
     process = CrawlerProcess({
         'ITEM_PIPELINES': {
@@ -292,15 +421,15 @@ def run_scraper():
 
 
 if __name__ == '__main__':
-    print("=" * 60)
-    print("Medical Device Jobs Scraper - GITHUB READY")
-    print("=" * 60)
+    print("=" * 70)
+    print("Medical Device Jobs Scraper - Comprehensive US Companies")
+    print("=" * 70)
     print(f"Starting at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-    print(f"Scraping {len(MEDICAL_DEVICE_COMPANIES)} companies...")
-    print("=" * 60)
+    print(f"Scraping {len(MEDICAL_DEVICE_COMPANIES)} major medical device companies...")
+    print("=" * 70)
 
     run_scraper()
 
-    print("=" * 60)
+    print("=" * 70)
     print("Scraping complete!")
-    print("=" * 60)
+    print("=" * 70)
